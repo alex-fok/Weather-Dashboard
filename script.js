bd5817ad76feb7901e978e6ab41b5d0fbe9e8f7c
Alexander Fok <foktmalexander@gmail.com>
2020-12-13 00:42:26 -0800

    Add function for declaring string variables based on the received JSON file

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
-const updateCurrentWeather = (current) => {
+const updateWeatherDisplay = (weatherObj) => {
 
 }
 
-const updateWeatherForecast = (forecast) => {
+const getWeatherObj = (city, data) => {
+    const {current, daily} = data;
 
+    const getDate = (unixTime) => new Date(parseInt(unixTime)*1000).toLocaleDateString('en-US');
+
+    const getForecast = (days) => days.map(day => {
+        return({
+            "date": getDate(day.dt),
+            "icon": `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`,
+            "temp": `${day.temp.day} \u2109`,
+            "humidity": `${day.humidity} %`
+        })
+    });
+
+    return ({
+        today: {
+            "name": city,
+            "date": getDate(current.dt),
+            "icon": `http://openweathermap.org/img/wn/${current.weather[0].icon}.png`,
+            "temp": `${current.temp} \u2109`,
+            "humidity": `${current.humidity} %`,
+            "windSpeed": `${current.wind_speed} MPH`,
+            "uvi": `${current.uvi}`
+        },
+        forecast: getForecast(daily.slice(1, 6))
+    })
+}
+
+const requestWeatherInfo = (coord, APIKey, resolve, reject) => {
+    const httpRequest = new XMLHttpRequest();
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=minutely,hourly,alerts&appid=${APIKey}&units=imperial`;
+
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState !== XMLHttpRequest.DONE) return;
+
+        httpRequest.status !== 200 ? reject("Weather info not available. Please try again.") : resolve(JSON.parse(httpRequest.responseText)); 
+    };
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
 }
 
 (() => {
-    let weatherInfo;
-
-    const requestWeatherInfo = (coord, APIKey, resolve) => {
-        const httpRequest = new XMLHttpRequest();
-        const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=minutely,hourly,alerts&appid=${APIKey}&units=imperial`;
-
-        const retrieveWeather = () => {
-            if (httpRequest.readyState !== XMLHttpRequest.DONE)
-                return;
-            httpRequest.status !== 200 ? resolve(null) : resolve(JSON.parse(httpRequest.responseText));
-        }
-        httpRequest.onreadystatechange = retrieveWeather;
-        httpRequest.open("GET", url, true);
-        httpRequest.send();
-    }
-    
     document.getElementById("searchBtn").addEventListener("click", () => {
-        const city = document.getElementById("searchInput").value.toLocaleLowerCase("en-us");
-        const {APIKey} = config;
+        const city = document.getElementById("searchInput").value.toLocaleLowerCase("en-us").trim();
         
         if (!cities[city]) return;
-        new Promise((resolve) =>{requestWeatherInfo(cities[city], APIKey, resolve)
+
+        new Promise((resolve) =>{
+            requestWeatherInfo(cities[city], config.APIKey, resolve, reject);
         }).then(data => {
-            const date = new Date(parseInt(data.current.dt)*1000).toLocaleDateString('en-US');
-            console.log(date);
-            
+            updateWeatherDisplay(getWeatherObj(city, data));
+        }).catch(msg => {
+            console.log(msg);
         });
     });
-    
 })();
